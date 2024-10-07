e9fe2cbb1d52e411e4667f10f7d3a9fc95f404b5
student <student@asturums-iMac-2.local>
2024-10-07 16:20:13 +0500

    Swagger

--- config/settings.py
+++ config/settings.py
@@ -38,20 +38,9 @@ INSTALLED_APPS = [
     'django.contrib.messages',
     'django.contrib.staticfiles',
     'rest_framework',
-    'rest_framework_simplejwt',
-    'blog',
+    'main',
+    'drf_yasg',
 ]
-
-AUTH_USER_MODEL = 'blog.CustomUser'
-
-REST_FRAMEWORK = {
-    'DEFAULT_AUTHENTICATION_CLASSES': [
-        'rest_framework_simplejwt.authentication.JWTAuthentication',
-    ],
-    'DEFAULT_PERMISSION_CLASSES': [
-        'rest_framework.permissions.IsAuthenticated',
-    ],
-}
 
 
 MIDDLEWARE = [
